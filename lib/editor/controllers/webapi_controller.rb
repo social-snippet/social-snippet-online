276f80c2519f2099afb466ea8e42012934efc236
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-18 17:47:07 +0900

    webapi: add error handling if source not found case

--- lib/editor/controllers/webapi_controller.rb
+++ lib/editor/controllers/webapi_controller.rb
@@ -11,19 +11,29 @@ require "rack/parser"
   end
 
   post :sources, :provides => :json do
-    source = ::Editor::Source.create(:text => params["text"])
+    source = ::Editor::Source.create!(:text => params["text"])
     source.as_json.to_json
   end
 
   put :sources, :with => [:id] do
-    source = ::Editor::Source.find(params[:id])
-    source.update_attribute :text, params["text"] unless params["text"].nil?
-    source.as_json.to_json
+    begin
+      source = ::Editor::Source.find(params[:id])
+      source.update_attribute :text, params["text"] unless params["text"].nil?
+      source.as_json.to_json
+    rescue ::Mongoid::Errors::DocumentNotFound => e
+      status 404
+      "error"
+    end
   end
   
   get :sources, :with => [:id] do
-    source = ::Editor::Source.find(params[:id])
-    source.as_json.to_json
+    begin
+      source = ::Editor::Source.find(params[:id])
+      source.as_json.to_json
+    rescue ::Mongoid::Errors::DocumentNotFound => e
+      status 404
+      "error"
+    end
   end
 
 end
