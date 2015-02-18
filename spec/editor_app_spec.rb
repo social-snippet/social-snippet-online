685caf98d3cd76d02547dcc233ed7762faa7ddf0
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-18 15:18:57 +0900

    test: add tests for padrino routing

--- spec/editor_app_spec.rb
+++ spec/editor_app_spec.rb
@@ -12,5 +12,29 @@ describe ::Editor::Application do
     it { should be_ok }
   end
 
+  context "GET /webapi/sources" do
+    before { get "/webapi/sources" }
+    subject { last_response }
+    it { should be_ok }
+  end
+
+  context "GET /webapi/sources/123" do
+    before { get "/webapi/sources/123" }
+    subject { last_response }
+    it { should be_ok }
+  end
+
+  context "POST /webapi/sources" do
+    before { post "/webapi/sources" }
+    subject { last_response }
+    it { should be_ok }
+  end
+
+  context "PUT /webapi/sources/123" do
+    before { put "/webapi/sources/123" }
+    subject { last_response }
+    it { should be_ok }
+  end
+
 end
 
