d1164120b783c42429b2a70efa302e1d3baa30bb
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-18 16:42:00 +0900

    webapi: use rack parser to parse json params

--- lib/editor/controllers/webapi_controller.rb
+++ lib/editor/controllers/webapi_controller.rb
@@ -1,9 +1,18 @@
+require "json"
+require "rack/parser"
+
 ::Editor::Application.controller :webapi do
+
+  use ::Rack::Parser, :parsers => {
+    "application/json" => proc {|data| ::JSON.parse data }
+  }
 
   get :sources do
   end
 
-  post :sources do
+  post :sources, :provides => :json do
+    source = ::Editor::Source.create(:text => params["text"])
+    source.as_json.to_json
   end
 
   put :sources, :with => [:id] do
