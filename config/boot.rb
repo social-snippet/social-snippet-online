ffc0a5d98adba0d8c4e72d5ce2e0e46319492f8d
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-18 15:35:23 +0900

    editor: load mongoid

--- config/boot.rb
+++ config/boot.rb
@@ -2,6 +2,8 @@ RACK_ENV = ENV["RACK_ENV"] ||= "development" unless defined?(RACK_ENV)
 PADRINO_ROOT = ::File.expand_path("../..", __FILE__) unless defined?(PADRINO_ROOT)
 
 require "padrino"
+require "mongoid"
+require "slim"
 
 ::Padrino.configure_apps do
 end
