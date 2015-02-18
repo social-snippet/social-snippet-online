ea5d84027cad5d35c8e759c91f12acfe4de7f41c
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-18 15:44:19 +0900

    test: add helper scripts

---
+++ spec/helpers/rack_test_helper.rb
@@ -0,0 +1,8 @@
+#
+# load rack-test
+#
+require "rack/test"
+::RSpec.configure do |conf|
+  conf.include ::Rack::Test::Methods
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
+require_relative "helpers/rack_test_helper"
+require_relative "helpers/database_cleaner_helper"
+require_relative "helpers/factory_girl_helper"
+
 RACK_ENV = "test" unless defined?(RACK_ENV)
 require_relative "../config/boot"
 
-#
-# load rack-test
-#
-require "rack/test"
-::RSpec.configure do |conf|
-  conf.include ::Rack::Test::Methods
-end
