b5336332f53b4ea3d20cb3b1ab6d5bdff43069a1
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-23 10:30:49 +0900

    source: use mongoid_token

--- lib/editor/models/source.rb
+++ lib/editor/models/source.rb
@@ -3,15 +3,20 @@ module Editor::Models
   class Source
 
     include ::Mongoid::Document
+    include ::Mongoid::Token
 
     field :text, :type => String
     field :language, :type => Integer, :default => 1
 
     def as_json(options = {})
       attrs = super(options)
-      attrs["id"] = attrs["_id"].to_s
+      attrs["id"] = attrs["token"].to_s
+      attrs.delete "_id"
+      attrs.delete "token"
       attrs
     end
+
+    token :length => 11
 
   end
 
